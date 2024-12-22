299c61bedc9a60a296a015e452e73a235ad560d6
Nwachukwu prince <bldrnwagod@gmail.com>
2024-12-22 05:25:03 -0800

    Code refacture and Tailwind Css installation

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='bg-red-400 text-'>Hello world</h1>
+      <p>please keep me out of your troubles</p>
+      <h4>This thing is funny ooo</h4>
+      <h3>This project is stubborn</h3>
+    </>
+  )
+}
+export default App;
