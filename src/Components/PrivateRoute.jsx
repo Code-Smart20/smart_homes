3436b301e976186115616d95bf14ee8ff24b1857
Nwachukwu prince <bldrnwagod@gmail.com>
2025-02-06 06:31:11 -0800

    fix typo error at privateRoute

--- src/Components/PrivateRoute.jsx
+++ src/Components/PrivateRoute.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
-import { UseAuthStatus } from '../hooks/UseAuthStatus';
+import { useAuthStatus } from '../hooks/useAuthStatus';
 import Spinner from './Spinner';
 
 const PrivateRoute = () => {
